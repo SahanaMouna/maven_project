1e3c6e95b6697ae61c0cb41d80be3020dc6f0e54
unknown <mounagowda150@gmail.com>
2024-11-19 12:03:58 +0530

    first commit

---
+++ src/main/java/sel_frame/Frame.java
@@ -0,0 +1,11 @@
+package sel_frame;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Frame {
+	@Test
+	public static void basic() {
+		ChromeDriver driver=new ChromeDriver();
+	
+	}
+}
